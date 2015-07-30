b00ce18a365e3b2436926e47a1379616eb77a8f8
tienquang <oclockvn@gmail.com>
2015-07-30 10:46:53 +0700

    update

---
+++ debt-fe/Controllers/ProfileController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace debt_fe.Controllers
+{
+    public class ProfileController : Controller
+    {
+        // GET: Profile
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ContactInformation()
+        {
+            return View();
+        }
+    }
+}
