409e1348e505178b7be723dedf9bf1cd1f7c5d74
quynhlx <quynhlx1412@gmail.com>
2015-08-19 17:15:51 +0700

    Update

--- debt-fe/Controllers/DocumentController.cs
+++ debt-fe/Controllers/DocumentController.cs
@@ -574,7 +574,7 @@ namespace debt_fe.Controllers
             var host = Request.Url.Host;           
             var redirect = string.Format("{0}://{1}/{2}",scheme,host.TrimEnd('/'), urlRedirect.TrimStart('/'));
 
-            redirect = string.Format("http://localhost:{0}/{1}", Request.Url.Port, urlRedirect);
+            //redirect = string.Format("http://localhost:{0}/{1}", Request.Url.Port, urlRedirect);
 
             // http://localhost:47854/Debt/Index
 
---
+++ debt-fe/Html/SendEmail.aspx.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Mail;
+using System.Text;
+using System.Collections.Specialized;
+
+public partial class SendEmail : System.Web.UI.Page
+{
+    protected NameValueCollection Config
+    {
+        get { return System.Configuration.ConfigurationManager.AppSettings; }
+    }
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Response.Clear();
+
+        if (!IsPostBack)
+        {
+            try
+            {
+                string sName = Request["name"];
+                string sEmail = Request["email"];
+                string sSubject = Request["subject"];
+                string sMessage = Request["message"];
+
+                if (sName != "" && sEmail != "")
+                {
+                    string sBody = "";
+                    sBody += "Name: " + sName + "<br />";
+                    sBody += "Email: " + sEmail + "<br />";
+                    sBody += "Subject: " + sSubject + "<br />";
+                    sBody += "Message: " + sMessage.Replace("\n", "<br />");
+
+                    this.SendMail(Config["EmailFrom"], Config["EmailTo"], Config["EmailSubject"], sBody);
+
+                    Response.Write("0");
+                }
+                else
+                    Response.Write("-1");
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
+        }
+
+        Response.End();
+    }
+
+    public int SendMail(string mailFrom, string mailTo, string subject, string content)
+    {
+        try
+        {
+            if (mailTo != string.Empty)
+            {
+                MailMessage msg = new MailMessage();
+                msg.To = mailTo;
+                msg.From = mailFrom;//ConfigurationSettings.AppSettings["mailFrom"];
+                msg.Body = content;
+                msg.Subject = subject;
+                msg.BodyEncoding = Encoding.UTF8;
+                msg.BodyFormat = MailFormat.Html;
+
+
+
+                try
+                {
+                    SmtpMail.SmtpServer = Config["SMTPServer"];
+                    SmtpMail.Send(msg);
+                }
+                catch (Exception ex)
+                {
+                }
+            }
+        }
+        catch (Exception)
+        { }
+        return 0;
+    }
+}
